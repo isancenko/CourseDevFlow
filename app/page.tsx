fe20e7ea9571a92c1dc2edead1d794ffd38111fb
isancenko <ilasancenko98@gmail.com>
2025-04-03 17:47:51 +0300

    Set up Light & Dark Theme, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,5 @@
-"use client";
-import { useEffect, useState } from "react";
-import { MdOutlineDarkMode } from "react-icons/md";
-import { MdLightMode } from "react-icons/md";
-
 const Home = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const isDark = localStorage.getItem("theme") === "dark";
-    setDarkMode(isDark);
-    if (isDark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, []);
-
-  const toggleDarkMode = () => {
-    const newMode = !darkMode;
-    setDarkMode(newMode);
-    if (newMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  };
-
-  return (
-    <div>
-      <h1 className="background-light900_dark200">Hello</h1>
-      <h1 className="background-light900_dark200">Hello</h1>
-      <button
-        onClick={toggleDarkMode}
-        className="mt-4 px-4 py-2 text-black rounded-lg cursor-pointer"
-      >
-        {darkMode ? <MdLightMode /> : <MdOutlineDarkMode />}
-      </button>
-    </div>
-  );
+  return <div>Hi</div>;
 };
 
 export default Home;
