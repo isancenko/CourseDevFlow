e13ca6636349167854d7359911137eef03ab1109
isancenko <ilasancenko98@gmail.com>
2025-04-11 16:34:14 +0300

    Implement/Update Database Models

---
+++ database/interaction.model.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  actionId: Types.ObjectId;
+  action: string;
+  actionType: string;
+}
+
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    action: { type: String, required: true },
+    actionType: { type: String, enum: ["question", "answer"], required: true },
+  },
+  { timestamps: true }
+);
+
+const Interaction =
+  models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
