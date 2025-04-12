25e770d62c469b22f91c57aaabc59db55016e863
isancenko <ilasancenko98@gmail.com>
2025-04-12 15:24:30 +0300

    Implement oAuth

--- constants/routes.ts
+++ constants/routes.ts
@@ -6,6 +6,7 @@ const ROUTES = {
   QUESTION: (id: string) => `questions/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
   ASK_QUESTION: "/ask-question",
+  SIGN_IN_WITH_OAUTH: `signin-with-oauth`,
 };
 
 export default ROUTES;
